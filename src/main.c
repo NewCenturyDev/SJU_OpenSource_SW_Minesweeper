2892c16c9128ad829875b35eaa3582137b2e270a
Park Jae-sung <flash246@naver.com>
2019-11-19 22:32:07 +0900

    cleancode3 - SET_VISI(x, y, s) 함수의 일반 함수화
     삼항 연산자의 사용으로 코드의 가독성이 떨어진다고 생각하였다.
    이 매크로 함수를 if문을 사용해 setVisi 일반 함수로 수정하였다.
    추가로 setMine의 자료형도 int에서 void로 교체하였다.

--- src/main.c
+++ src/main.c
@@ -21,7 +21,6 @@ char p[] = { ' ', 'O', 'X', '_' };
 #define MINE 2
 #define QUERY 3
 
-#define SET_VISI(x, y, s) M(x, y)=s?M(x, y)|2:M(x, y)&254
 #define SET_MARK(x, y, s) M(x, y)=s&2?s&1?M(x, y)|12:(M(x, y)&243)|8:s&1?M(x, y)&243|4:M(x, y)&243
 #define INC_NUM(x, y) if (!IS_OUT(x, y)) M(x, y)=(M(x, y)&15)|(GET_NUM(x, y)+1)<<4
 
@@ -53,13 +52,22 @@ int areaMineNum(int x, int y) {
 	return result;
 }
 
-int setMine(x, y, s) {
+void setMine(x, y, s) {
 	if (s == true) {
 		areaInfo[x][y].isMine = 1;
 	}
 	if (s == false) {
 		areaInfo[x][y].isMine = 1;
 	} //todo: s가 참, 거짓의 값과 상관없이 같은 연산을 함. 검토 후 수정이 필요해 보임
+}
+
+void setVisi(x, y, s) {
+	if (s == true) {
+		areaInfo[x][y].isVisi = 1;
+	}
+	if (s == false) {
+		areaInfo[x][y].isMine = 1;
+	}
 }
 
 //입력된 좌표가 범위 밖인지 판단
